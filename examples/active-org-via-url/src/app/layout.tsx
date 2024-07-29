54ea34e8b5323b84675c2f4a2f698e73afc7f445
Kyle MacDonald <kyle@clerk.dev>
2024-07-29 16:45:35 -0400

    check point

--- examples/active-org-via-url/src/app/layout.tsx
+++ examples/active-org-via-url/src/app/layout.tsx
@@ -1,7 +1,15 @@
 import type { Metadata } from "next";
+import { Button } from "@/components/ui/button";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import {
+  ClerkProvider,
+  OrganizationSwitcher,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +26,31 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <main>
+            <header className="w-full bg-muted py-4">
+              <div className="container mx-auto flex items-center justify-between px-4">
+                <div className="text-lg font-medium">
+                  Orgs: {"<Name of Example>"}
+                </div>
+                <SignedOut>
+                  <SignInButton>
+                    <Button variant="outline">Sign In with Clerk</Button>
+                  </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                  <OrganizationSwitcher />
+                  <UserButton />
+                </SignedIn>
+              </div>
+            </header>
+            <section className="container mx-auto px-4 py-8 max-w-10xl">
+              <div className="items-center font-mono text-sm pb-24">
+                {children}
+              </div>
+            </section>
+          </main>
+        </body>
       </html>
     </ClerkProvider>
   );
--- examples/active-org-via-url/src/app/page.tsx
+++ examples/active-org-via-url/src/app/page.tsx
@@ -2,18 +2,13 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <SignedOut>
-          <SignInButton>
-            <button>Sign in with Clerk</button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <p>This is our {"<UserButton />"}</p>
-          <UserButton />
-        </SignedIn>
-      </div>
-    </main>
+    <>
+      <SignedIn>
+        <p className="pb-8">You are signed in</p>
+      </SignedIn>
+      <SignedOut>
+        <p className="pb-8">You are signed out</p>
+      </SignedOut>
+    </>
   );
 }
