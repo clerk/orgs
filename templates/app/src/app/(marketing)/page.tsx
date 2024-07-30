9c629c48aaa1159e2b15eb0e95d148ecc2da13eb
Kyle MacDonald <kyle@clerk.dev>
2024-07-30 10:47:54 -0400

    clean up dashboard layout

--- templates/app/src/app/(marketing)/page.tsx
+++ templates/app/src/app/(marketing)/page.tsx
@@ -4,18 +4,6 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <>
-      {/* <SignedIn>
-        <p>You are signed in but on the marketing page</p>
-        <Link href="/dashboard">Head to the Dashboard</Link>
-        <p>
-          <UserButton />
-        </p>
-      </SignedIn>
-      <SignedOut>
-        <h2>Marketing Site</h2>
-        <p>You are signed out</p>
-        <SignInButton />
-      </SignedOut> */}
       <h1 className="text-3xl font-bold">Home</h1>
       <p className="font-xs font-mono">This is your marketing homepage</p>
       <p className="font-xs font-mono">
