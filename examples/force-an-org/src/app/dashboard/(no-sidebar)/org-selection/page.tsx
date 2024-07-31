cc41f22babc61cb557bd0f6de6e158f18b652fad
Kyle MacDonald <kyle@clerk.dev>
2024-07-31 07:49:35 -0400

    add path note in org selection view

--- examples/force-an-org/src/app/dashboard/(no-sidebar)/org-selection/page.tsx
+++ examples/force-an-org/src/app/dashboard/(no-sidebar)/org-selection/page.tsx
@@ -14,10 +14,14 @@ export default async function OrgSelectionPage() {
     <div className="w-full py-12 md:py-24 lg:py-32">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
-          <h2 className="text-lg pb-8">
-            Please select an organization, or create a new one to continue:
-          </h2>
+          <h2 className="text-lg">Please create or select an organization</h2>
           <OrganizationList hidePersonal />
+          <p className="pt-12">
+            This org selection page exists inside your app:
+          </p>
+          <p className="pt-4 text-xs font-mono text-gray-500">
+            src/app/dashboard/(no-sidebar)/org-selection/page.tsx
+          </p>
         </div>
       </div>
     </div>
