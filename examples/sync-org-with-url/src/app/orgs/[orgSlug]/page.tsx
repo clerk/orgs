b0b6488aeb6cad407bffd2bff81933490854cbd8
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2024-08-06 11:13:57 -0400

    Printing the org slug from `auth()` in a server component

--- examples/sync-org-with-url/src/app/orgs/[orgSlug]/page.tsx
+++ examples/sync-org-with-url/src/app/orgs/[orgSlug]/page.tsx
@@ -1,3 +1,11 @@
+import { auth } from '@clerk/nextjs/server';
+
 export default function Home() {
-  return <p className="pb-8">The landing page for your org</p>;
+  const {orgSlug} = auth();
+
+  return (
+   <>
+     <p className="pb-8">From auth(), I know your org slug is: {orgSlug}</p>
+   </>
+  )
 }
