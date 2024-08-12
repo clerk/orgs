6845537662033ec8dfa2ec9be01a431ff1b991a2
Nicolas Lopes <nicolas@clerk.dev>
2024-08-12 10:36:14 -0300

    feat: add 'via API' section

--- examples/limit-org-creation/src/app/dashboard/[[...rest]]/page.tsx
+++ examples/limit-org-creation/src/app/dashboard/[[...rest]]/page.tsx
@@ -11,15 +11,39 @@ import {
   useOrganizationList,
   useUser,
 } from "@clerk/nextjs";
+import { useState } from "react";
+import { toast, Toaster } from "sonner";
 
 export default function Dashboard() {
   const { user, isLoaded } = useUser();
   const { isLoaded: isMembershipLoaded } = useOrganizationList();
+  const [isUpdating, setIsUpdating] = useState(false);
+
   if (!isLoaded || !isMembershipLoaded) return <ClerkLoading />;
   console.log(user);
+
+  const onChangeOrgCreation = async (enable: boolean) => {
+    setIsUpdating(true);
+    try {
+      await fetch("/api/org-creation", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ enable }),
+      });
+      user?.reload();
+    } catch (error) {
+      toast.error(`Error updating org creation status: ${error}`);
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col gap-4">
+        <Toaster richColors position="bottom-right" />
         <h3 className="text-xl font-bold mt-8">Current User:</h3>
         <p>
           <strong>User ID:</strong> {user?.id}
@@ -30,10 +54,11 @@ export default function Dashboard() {
             {" "}
             {user?.createOrganizationEnabled ? "allowed" : "not allowed"}
           </strong>{" "}
-          to create organizations
+          to create organizations. Limit is set to{" "}
+          <strong>{user?.createOrganizationsLimit}</strong>
         </p>
-        <p className="mt-8">
-          There are a few ways to test this functionality:
+        <div className="mt-8">
+          <p>There are a few ways to test this functionality:</p>
           <ol className="list-disc pt-4">
             <li className="ml-8">
               Override this{" "}
@@ -54,8 +79,22 @@ export default function Dashboard() {
                 Dashboard / Organization Settings
               </a>
             </li>
+
+            <li className="ml-8">
+              via API:{" "}
+              <button
+                disabled={isUpdating}
+                className="text-purple-600 hover:underline disabled:cursor-not-allowed disabled:opacity-50"
+                onClick={async () =>
+                  onChangeOrgCreation(!user?.createOrganizationEnabled)
+                }
+              >
+                {user?.createOrganizationEnabled ? "Disable" : "Enable"}
+              </button>{" "}
+              org creation for this user
+            </li>
           </ol>
-        </p>
+        </div>
         <hr />
         <h3 className="text-xl font-bold mt-8">
           Use these components to test for the current user:
