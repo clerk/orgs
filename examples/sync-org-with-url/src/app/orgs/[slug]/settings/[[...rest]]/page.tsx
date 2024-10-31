d64732d01d3ab4b0e76f9ea38a9f6fa5b883854c
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2024-10-31 09:47:33 -0400

    Adding loading state to client-side components.

--- examples/sync-org-with-url/src/app/orgs/[slug]/settings/[[...rest]]/page.tsx
+++ examples/sync-org-with-url/src/app/orgs/[slug]/settings/[[...rest]]/page.tsx
@@ -5,9 +5,17 @@ import {OrganizationList, OrganizationProfile, useOrganization} from "@clerk/nex
 export default function Home({params}:{
   params: { slug: string }
 }) {
-  const {organization} = useOrganization()
+  const {organization, isLoaded} = useOrganization()
+
+  if (!isLoaded) {
+    return <>Loading...</>
+  }
 
   if (!organization || organization.slug != params.slug) {
+    console.log("Organization:", organization)
+    console.log("Organization Slug:", organization?.slug)
+    console.log("Params slug:", params.slug)
+
     return (
       <>
         <p className="pb-8">Sorry, organization {params.slug} is not valid.</p>
