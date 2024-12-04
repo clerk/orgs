18a57193a447dbc08b047188003a0d91b61e3f8d
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2024-12-04 11:52:33 -0500

    Upgrading sync-org-with-url dependencies

--- examples/sync-org-with-url/src/app/page.tsx
+++ examples/sync-org-with-url/src/app/page.tsx
@@ -1,10 +1,10 @@
 import {auth} from "@clerk/nextjs/server";
 import {redirect} from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
   // Nothing to display at the root. If the personal account is active,
   // redirect to /me, otherwise redirect to the org home.
-  let {orgSlug} = auth();
+  let {orgSlug} = await auth();
   if (orgSlug) {
     // An organization is active - redirect to the org home.
     redirect(`/orgs/${orgSlug}`)
