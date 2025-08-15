a77f7e687a101579c6a2e74d8dde224c3406db2c
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2025-08-15 12:31:24 -0300

    Add example of force-an-org with custom flows + clerk-js  (#30)
    * Add application
    
    * Update task key to `choose-organization`
    
    * Bump clerk-js

---
+++ examples/force-an-org-custom-flows/src/elements.js
@@ -0,0 +1,31 @@
+const loadingEl = document.getElementById("loading");
+const authContainerEl = document.getElementById("auth-container");
+const dashboardEl = document.getElementById("dashboard");
+const signInEl = document.getElementById("sign-in");
+const signUpEl = document.getElementById("sign-up");
+const orgCreationEl = document.getElementById("org-creation");
+const signInForm = document.getElementById("sign-in-form");
+const signUpForm = document.getElementById("sign-up-form");
+const orgCreationForm = document.getElementById("org-creation-form");
+const toggleToSignUpBtn = document.getElementById("toggle-to-sign-up");
+const toggleToSignInBtn = document.getElementById("toggle-to-sign-in");
+const userInfoEl = document.getElementById("user-info");
+const orgInfoEl = document.getElementById("org-info");
+const signOutBtn = document.getElementById("sign-out-btn");
+
+export {
+  authContainerEl,
+  dashboardEl,
+  loadingEl,
+  orgCreationEl,
+  orgCreationForm,
+  orgInfoEl,
+  signInEl,
+  signInForm,
+  signOutBtn,
+  signUpEl,
+  signUpForm,
+  toggleToSignInBtn,
+  toggleToSignUpBtn,
+  userInfoEl,
+};
